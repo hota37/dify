38b2e383634c9a4db6b740672640926d926b89ac
takatost <takatost@gmail.com>
2024-03-04 13:32:59 +0800

    fix typo

--- api/core/agent/cot_agent_runner.py
+++ api/core/agent/cot_agent_runner.py
@@ -133,7 +133,7 @@ class CotAgentRunner(BaseAgentRunner):
             )
 
             # recale llm max tokens
-            self.recale_llm_max_tokens(self.model_config, prompt_messages)
+            self.recalc_llm_max_tokens(self.model_config, prompt_messages)
             # invoke model
             chunks: Generator[LLMResultChunk, None, None] = model_instance.invoke_llm(
                 prompt_messages=prompt_messages,
